3750aadf43695834eddf03b862fe80dcf01cd271
UK-Dev786 <ukdev.codetivelab@gmail.com>
2025-04-23 15:18:40 +0500

    remove gftr gray section and make it same like the first one page.

--- lib/View/Screens/allaboutUs6.dart
+++ lib/View/Screens/allaboutUs6.dart
@@ -22,28 +22,22 @@ class _AllAboutUs06State extends State<AllAboutUs06> {
                 fit: BoxFit.fill)),
         child: Column(
           children: [
+            Container(
+              height: screenHeight(context, dividedBy: 4.6),
+              width: screenWidth(context, dividedBy: 2),
+              child: Image.asset(
+                ImageConstants.textWithLogo,
+              ),
+            ),
             Padding(
               padding:
                   EdgeInsets.only(top: screenHeight(context, dividedBy: 25)),
               child: customText(
-                  'The Group', Colors.white, 18, FontWeight.bold, poppins),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.only(top: screenHeight(context, dividedBy: 80)),
-              height: screenHeight(context, dividedBy: 2.5),
-              width: screenWidth(context, dividedBy: 1),
-              alignment: Alignment.center,
-              color: ColorCodes.lightGrey,
-              child: Container(
-                width: screenWidth(context, dividedBy: 1.7),
-                child: customText(
-                    "Video of adding friends from contact list, seeing the GFTR Group page, clicking on one user, seeing their saved gift ideas and selecting one to buy.",
-                    Colors.black,
-                    13,
-                    FontWeight.w400,
-                    poppins,
-                    isTextAlign: true),
+                'The Group',
+                Colors.white,
+                25,
+                FontWeight.w600,
+                poppins,
               ),
             ),
             Container(
@@ -54,7 +48,7 @@ class _AllAboutUs06State extends State<AllAboutUs06> {
                   " Create your GFTR Group of Friends & Family you’d like to get gifts for (or from!), then check out their Gftr Wishlists to see what they’ve got their eye on.",
                   Colors.white,
                   13,
-                  FontWeight.w400,
+                  FontWeight.w500,
                   poppins,
                   isTextAlign: true),
             ),
@@ -62,8 +56,40 @@ class _AllAboutUs06State extends State<AllAboutUs06> {
               padding:
                   EdgeInsets.only(top: screenHeight(context, dividedBy: 30)),
               child: customText("And, with a single click, give that gift!",
-                  Colors.white, 13, FontWeight.w400, poppins,
+                  Colors.white, 13, FontWeight.w500, poppins,
                   isTextAlign: true),
+            ),
+            SizedBox(height: screenHeight(context, dividedBy: 20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                customText(
+                  "You’re a",
+                  Colors.white,
+                  16,
+                  FontWeight.w500,
+                  poppins,
+                  isTextAlign: true,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Image.asset(
+                  ImageConstants.gftrBlack,
+                  color: Colors.white,
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                customText(
+                  "now",
+                  Colors.white,
+                  16,
+                  FontWeight.w500,
+                  poppins,
+                ),
+              ],
             ),
           ],
         ),
